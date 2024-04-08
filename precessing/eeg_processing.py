e29c4e2b163141ad2f11e578312aa43d473664aa
bayo.linkedin@outlook.fr <habibbayo@Habib>
2024-04-08 04:36:30 -0400

    move methods from eeg_preprocessor to eeg_processing

--- precessing/eeg_processing.py
+++ precessing/eeg_processing.py
@@ -7,3 +7,39 @@ def band_power():
 def feature_extraction():
 
     pass
+
+def remove_artifacts(self):
+    """
+    Remove artifacts from the EEG data.
+    """
+    pass
+    #logger.info("Removing artifacts")
+    # Implement artifact removal here
+    #self.__preprocessed_signal = None # receive the processed signal after removing artifacts
+
+def set_bad_channels(self, bad_channels):
+    """
+    Set the list of bad channels.
+
+    Parameters:
+        bad_channels (list): List of bad channels.
+    """
+    pass
+    #logger.info(f"Setting bad channels to {bad_channels}")
+    #self.raw.info['bads'] = bad_channels
+
+def get_bad_channels(self):
+        """
+        Get the list of bad channels.
+        """
+        pass
+        #logger.info("Getting bad channels")
+        #return self.raw.info['bads']
+
+def get_artifacts(self):
+        """
+        Get the list of artifacts.
+        """
+        pass
+        #logger.info("Getting artifacts")
+        # Implement artifact detection here
